9e7cdc91603c8c55f0b0c8aa20c2931a4c0c4294
razputshivanshu <razputshivanshu@gmail.com>
2025-04-23 13:43:21 +0530

    learnt about Ecmascript

---
+++ 01basics/01_variable.js
@@ -0,0 +1,18 @@
+const accountId = 144553;
+let accountEmail = "abc@gmail.com";
+var accountPassword = "abc@cdsd";
+accountCity = "Jaipur";
+let accountState;
+
+
+// accountId = 2; //not allowed to change the const variable
+// console.log(accountId);
+
+
+accountEmail = "xyz@gmail.com";
+accountPassword = "212121";
+accountCity = "banglore";
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
+
---
+++ 01basics/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; //treat all JS code as newer version of JS.
+
+// alert("hello"); // We are using Node.js not browser
+
+
+console.log(3 + 3); console.log("Hitesh");
+
+
+
+/// Primitive DataTypes
+// number
+// bigInt
+// String =>""
+// boolean => true / false
+// null => standalone value & it means empty value. Let's suppose we are building an temperature application and server has an issue and it has sent 0 deg but 0 is a value so we don't want to use it so we can use null.
+// undefined => it is given when no value is assigned
+// symbol ==> used for uniqueness
+
+
+/// Objects
+console.log(typeof (undefined));
