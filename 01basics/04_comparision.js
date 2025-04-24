55c2111a9a1a21e0fb651ff69c30c9e708a927f3
razputshivanshu <razputshivanshu@gmail.com>
2025-04-24 11:15:56 +0530

    Learnt about comparision operators

---
+++ 01basics/04_comparision.js
@@ -0,0 +1,23 @@
+// console.log(2 > 1);
+// console.log(2 == 1);
+
+// console.log(2 + 2 < 10);
+// console.log(2 != 3);
+
+//Above one are pretty basic to guess but the problem occurs when we are comparing with  === 
+
+
+// The Js automatically converts string into number
+// console.log("02" > 1);
+
+
+//Good Practice: Make sure that whenever you are comparing two values it should be in same datatye it will prevent us from bugs and error when we are creating large size applications.
+// console.log(null == 0);
+// console.log(null > 0);
+// console.log(null >= 0);
+// console.log(undefined == 0);
+
+
+
+// '===' checks the datatypes as well as values.
+console.log("2" === 2);
